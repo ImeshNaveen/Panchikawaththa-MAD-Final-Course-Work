bf2ca3a2283477b84cd2150e00aaeaf161b2ffa2
Pathum Theekshana <kptpremawansha@students.nsbm.ac.lk>
2025-03-15 11:16:47 +0530

    add register

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'pages/login_page.dart';
+import 'pages/sign_up.dart';
 
 void main() => runApp(MaterialApp(
       debugShowCheckedModeBanner: false,
       home: HomePage(),
+      //home: MyApp(),
     ));
---
+++ lib/pages/sign_up.dart
@@ -0,0 +1,418 @@
+import 'package:flutter/material.dart';
+import 'login_page.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    Color myGreenColor = Color(0xFF02B91A);
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          width: double.infinity,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                myGreenColor,
+                Colors.green[200]!,
+              ],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+            ),
+          ),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: 80,
+                ),
+                Padding(
+                  padding: EdgeInsets.all(20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "SignUp",
+                        style: TextStyle(color: Colors.white, fontSize: 40),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        "Register your account",
+                        style: TextStyle(color: Colors.white, fontSize: 18),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(60),
+                      topRight: Radius.circular(60),
+                    ),
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.all(20),
+                    child: Column(
+                      children: [
+                        SizedBox(
+                          height: 60,
+                        ),
+                        Container(
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(30),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.green[200]!,
+                                blurRadius: 20,
+                                offset: Offset(0, 10),
+                              ),
+                            ],
+                          ),
+                          child: Column(
+                            children: [
+                              Container(
+                                padding: EdgeInsets.all(8),
+                                child: TextField(
+                                  decoration: InputDecoration(
+                                    hintText: "First Name",
+                                    hintStyle: TextStyle(color: Colors.grey),
+                                    border: InputBorder.none,
+                                    contentPadding: EdgeInsets.only(left: 10),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Container(
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(30),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.green[200]!,
+                                blurRadius: 20,
+                                offset: Offset(0, 10),
+                              ),
+                            ],
+                          ),
+                          child: Column(
+                            children: [
+                              Container(
+                                padding: EdgeInsets.all(8),
+                                child: TextField(
+                                  decoration: InputDecoration(
+                                    hintText: "Last Name",
+                                    hintStyle: TextStyle(color: Colors.grey),
+                                    border: InputBorder.none,
+                                    contentPadding: EdgeInsets.only(left: 10),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Container(
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(30),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.green[200]!,
+                                blurRadius: 20,
+                                offset: Offset(0, 10),
+                              ),
+                            ],
+                          ),
+                          child: Column(
+                            children: [
+                              Container(
+                                padding: EdgeInsets.all(8),
+                                child: TextField(
+                                  decoration: InputDecoration(
+                                    hintText: "Email",
+                                    hintStyle: TextStyle(color: Colors.grey),
+                                    border: InputBorder.none,
+                                    contentPadding: EdgeInsets.only(left: 10),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Container(
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(30),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.green[200]!,
+                                blurRadius: 20,
+                                offset: Offset(0, 10),
+                              ),
+                            ],
+                          ),
+                          child: Column(
+                            children: [
+                              Container(
+                                child: PasswordField(),
+                              ),
+                            ],
+                          ),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Container(
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(30),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.green[200]!,
+                                blurRadius: 20,
+                                offset: Offset(0, 10),
+                              ),
+                            ],
+                          ),
+                          child: Column(
+                            children: [
+                              Container(
+                                child: ConfirmPasswordField(),
+                              ),
+                            ],
+                          ),
+                        ),
+                        SizedBox(
+                          height: 40,
+                        ),
+                        Text(
+                          "Fill those details to create an account.",
+                          style: TextStyle(color: Colors.grey),
+                        ),
+                        SizedBox(
+                          height: 25,
+                        ),
+                        ElevatedButton(
+                          onPressed: () {
+                            // TODO: Handle Sign Up button press
+                            print("Sign Up button clicked!");
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor:
+                                Color(0xFF02B91A), // Custom Green Color
+                            shape: RoundedRectangleBorder(
+                              borderRadius:
+                                  BorderRadius.circular(30), // Rounded edges
+                            ),
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 60, vertical: 15),
+                          ),
+                          child: Text(
+                            "Sign Up",
+                            style: TextStyle(fontSize: 18, color: Colors.white),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Text("or SignUp with",
+                            style: TextStyle(color: Colors.grey)),
+                        SizedBox(
+                          height: 15,
+                        ),
+                        // Social Media Sign-In Buttons
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            // Facebook Button
+                            GestureDetector(
+                              onTap: () {
+                                print("Facebook Sign-In clicked!");
+                              },
+                              child: SizedBox(
+                                width: 40, // Set icon width
+                                height: 40, // Set icon height
+                                child: Image.asset("assets/facebook.png"),
+                              ),
+                            ),
+
+                            SizedBox(width: 15), // Reduce spacing
+
+                            // Google (Gmail) Button
+                            GestureDetector(
+                              onTap: () {
+                                print("Google Sign-In clicked!");
+                              },
+                              child: SizedBox(
+                                width: 40,
+                                height: 40,
+                                child: Image.asset("assets/google.png"),
+                              ),
+                            ),
+
+                            SizedBox(width: 15),
+
+                            // iCloud Button
+                            GestureDetector(
+                              onTap: () {
+                                print("iCloud Sign-In clicked!");
+                              },
+                              child: SizedBox(
+                                width: 40,
+                                height: 40,
+                                child: Image.asset("assets/apple.png"),
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            // Navigate to Sign In page
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => HomePage()),
+                            );
+                          },
+                          child: Text(
+                            "I already have an account.",
+                            style: TextStyle(fontSize: 16, color: myGreenColor),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+// Password Field with Show/Hide Toggle
+class PasswordField extends StatefulWidget {
+  const PasswordField({super.key});
+
+  @override
+  _PasswordFieldState createState() => _PasswordFieldState();
+}
+
+class _PasswordFieldState extends State<PasswordField> {
+  bool _isObscured = true; // Initially hide password
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(8),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(30),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.green[200]!,
+            blurRadius: 20,
+            offset: const Offset(0, 10),
+          ),
+        ],
+      ),
+      child: TextField(
+        obscureText: _isObscured, // Toggle password visibility
+        decoration: InputDecoration(
+          hintText: "Password",
+          hintStyle: const TextStyle(color: Colors.grey),
+          border: InputBorder.none,
+          contentPadding:
+              EdgeInsets.only(left: 10, top: 9), // Added contentPadding
+          suffixIcon: IconButton(
+            icon: Icon(
+              _isObscured ? Icons.visibility : Icons.visibility_off,
+              color: Colors.grey,
+            ),
+            onPressed: () {
+              setState(() {
+                _isObscured = !_isObscured; // Toggle obscureText
+              });
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+// Confirm Password Field with Show/Hide Toggle
+class ConfirmPasswordField extends StatefulWidget {
+  const ConfirmPasswordField({super.key});
+
+  @override
+  _ConfirmPasswordFieldState createState() => _ConfirmPasswordFieldState();
+}
+
+class _ConfirmPasswordFieldState extends State<ConfirmPasswordField> {
+  bool _isObscured = true; // Initially hide password
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(8),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(30),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.green[200]!,
+            blurRadius: 20,
+            offset: const Offset(0, 10),
+          ),
+        ],
+      ),
+      child: TextField(
+        obscureText: _isObscured, // Toggle password visibility
+        decoration: InputDecoration(
+          hintText: "Confirm Password",
+          hintStyle: const TextStyle(color: Colors.grey),
+          border: InputBorder.none,
+          contentPadding:
+              EdgeInsets.only(left: 10, top: 9), // Added contentPadding
+          suffixIcon: IconButton(
+            icon: Icon(
+              _isObscured ? Icons.visibility : Icons.visibility_off,
+              color: Colors.grey,
+            ),
+            onPressed: () {
+              setState(() {
+                _isObscured = !_isObscured; // Toggle obscureText
+              });
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
