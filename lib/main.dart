f94514d3a623f7d51b5468c87d0c7b27fed61ced
Pathum Theekshana <kptpremawansha@students.nsbm.ac.lk>
2025-05-06 13:47:47 +0530

    chanaged

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:panchikawaththa/pages/Root.dart';
 
-void main() => runApp(
-  MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: RootPage(),
-    //home: MyApp(),
-  ),
-);
+void main() =>
+    runApp(MaterialApp(debugShowCheckedModeBanner: false, home: RootPage()));
