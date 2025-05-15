edda96ea3805317df5aaa45db84f2e447f7493b1
Naveen Thanthrige <118995408+ImeshNaveen@users.noreply.github.com>
2025-05-16 02:16:31 +0530

    Repair login page
    repair the UI of login page matching with signUp page.

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -5,59 +5,52 @@ import 'package:panchikawaththa/pages/sign_up.dart';
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
-  // Define the color as a variable
   static const Color primaryColor = Color.fromARGB(255, 20, 211, 3);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Container(
+    Color myGreenColor = Color(0xFF02B91A);
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Container(
           width: double.infinity,
+          height: double.infinity,
           decoration: BoxDecoration(
             gradient: LinearGradient(
+              colors: [myGreenColor, Colors.green[200]!],
               begin: Alignment.topCenter,
-              colors: [
-                primaryColor, // Use the variable here
-                primaryColor, // Use the variable here
-              ],
+              end: Alignment.bottomCenter,
             ),
           ),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              SizedBox(height: 40),
+              const SizedBox(height: 80),
               Padding(
-                padding: EdgeInsets.all(20),
+                padding: const EdgeInsets.all(20),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
+                  children: const [
                     Text(
-                      'Login',
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 40,
-                        fontWeight: FontWeight.bold,
-                        fontFamily: 'Poppins',
-                      ),
+                      "SignIn",
+                      style: TextStyle(color: Colors.white, fontSize: 40),
                     ),
                     SizedBox(height: 10),
                     Text(
-                      'Welcome Back',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                        fontFamily: 'Poppins',
-                      ),
+                      "Login to your account",
+                      style: TextStyle(color: Colors.white, fontSize: 18),
                     ),
                   ],
                 ),
               ),
-              SizedBox(height: 20),
+              const SizedBox(height: 30),
+              // Expanded white section
               Expanded(
                 child: Container(
-                  decoration: BoxDecoration(
+                  width: double.infinity,
+                  decoration: const BoxDecoration(
                     color: Colors.white,
                     borderRadius: BorderRadius.only(
                       topLeft: Radius.circular(60),
@@ -65,177 +58,137 @@ class LoginPage extends StatelessWidget {
                     ),
                   ),
                   child: Padding(
-                    padding: EdgeInsets.all(30),
-                    child: Column(
-                      children: [
-                        SizedBox(height: 60),
-                        Container(
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(10),
-                            boxShadow: [
-                              BoxShadow(
-                                color: primaryColor, // Use the variable here
-                                blurRadius: 20,
-                                offset: Offset(0, 10),
-                              ),
-                            ],
-                          ),
-                          child: Column(
-                            children: [
-                              Container(
-                                padding: EdgeInsets.all(10),
-                                decoration: BoxDecoration(
-                                  border: Border(
-                                    bottom: BorderSide(color: primaryColor),
-                                  ), // Use the variable here
+                    padding: const EdgeInsets.all(20),
+                    child: SingleChildScrollView(
+                      child: Column(
+                        children: [
+                          const SizedBox(height: 60),
+                          // Email Field
+                          Container(
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(30),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.green[200]!,
+                                  blurRadius: 20,
+                                  offset: Offset(0, 10),
                                 ),
-                                child: TextField(
-                                  decoration: InputDecoration(
-                                    hintText: "Email or Phone number",
-                                    hintStyle: TextStyle(color: Colors.black),
-                                    border: InputBorder.none,
-                                  ),
-                                ),
-                              ),
-                              SizedBox(height: 15), // Space between fields
-                              Container(
-                                padding: EdgeInsets.all(10),
-                                decoration: BoxDecoration(
-                                  border: Border(
-                                    bottom: BorderSide(color: primaryColor),
-                                  ), // Use the variable here
-                                ),
-                                child: TextField(
-                                  decoration: InputDecoration(
-                                    hintText: "Password",
-                                    hintStyle: TextStyle(color: Colors.black),
-                                    border: InputBorder.none,
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                        SizedBox(height: 40),
-                        Text(
-                          "Forgot Password?",
-                          style: TextStyle(color: Colors.grey),
-                        ),
-                        SizedBox(height: 40),
-                        Container(
-                          height: 50,
-                          margin: EdgeInsets.symmetric(horizontal: 50),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(50),
-                            color: primaryColor, // Use the variable here
-                          ),
-                          child: Center(
-                            child: Text(
-                              "Login",
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 15,
-                              ),
+                              ],
                             ),
-                          ),
-                        ),
-                        SizedBox(height: 20),
-                        GestureDetector(
-                          onTap: () {},
-                          child: RichText(
-                            text: TextSpan(
+                            child: Column(
                               children: [
-                                TextSpan(
-                                  text: "New Member? ",
-                                  style: TextStyle(
-                                    color: Colors.black,
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 16,
-                                  ),
-                                ),
-                                TextSpan(
-                                  recognizer:
-                                      TapGestureRecognizer()
-                                        ..onTap = () {
-                                          // Navigate to logIn page
-                                          Navigator.push(
-                                            context,
-                                            MaterialPageRoute(
-                                              builder:
-                                                  (context) => SignUpPage(),
-                                            ),
-                                          );
-                                        },
-                                  text: "Register",
-                                  style: TextStyle(
-                                    color: primaryColor,
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 16,
+                                Container(
+                                  padding: EdgeInsets.all(8),
+                                  child: TextField(
+                                    decoration: InputDecoration(
+                                      hintText: "Email",
+                                      hintStyle: TextStyle(color: Colors.grey),
+                                      border: InputBorder.none,
+                                      contentPadding: EdgeInsets.only(left: 10),
+                                    ),
                                   ),
                                 ),
                               ],
                             ),
                           ),
-                        ),
-                        SizedBox(height: 60),
-                        Row(
-                          mainAxisAlignment:
-                              MainAxisAlignment.center, // Align to center
-                          children: [
-                            Container(
-                              height: 50,
-                              width: 50, // Set width
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(50),
-                                color: const Color.fromARGB(
-                                  255,
-                                  255,
-                                  255,
-                                  255,
-                                ), // Google's login button is usually white
+                          const SizedBox(height: 20),
+                          // Password Field (Placeholder)
+                          Container(
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(30),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.green[200]!,
+                                  blurRadius: 20,
+                                  offset: Offset(0, 10),
+                                ),
+                              ],
+                            ),
+                            child: Column(
+                              children: [Container(child: PasswordField())],
+                            ),
+                          ),
+                          const SizedBox(height: 30),
+                          // Sign In Button
+                          ElevatedButton(
+                            onPressed: () {
+                              print("Sign In button clicked!");
+                            },
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: myGreenColor,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(30),
                               ),
-                              child: Center(
-                                child: Image.asset(
-                                  'assets/google.png', // Make sure this path is correct
-                                  height: 30, // Adjust size
-                                  width: 30,
+                              padding: const EdgeInsets.symmetric(
+                                horizontal: 60,
+                                vertical: 15,
+                              ),
+                            ),
+                            child: const Text(
+                              "Sign In",
+                              style:
+                                  TextStyle(fontSize: 18, color: Colors.white),
+                            ),
+                          ),
+                          const SizedBox(height: 20),
+                          const Text(
+                            "or SignIn with",
+                            style: TextStyle(color: Colors.grey),
+                          ),
+                          const SizedBox(height: 15),
+                          // Social Media Icons
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              GestureDetector(
+                                onTap: () => print("Facebook Sign-In clicked!"),
+                                child: SizedBox(
+                                  width: 40,
+                                  height: 40,
+                                  child: Image.asset("assets/facebook.png"),
                                 ),
                               ),
-                            ),
-                            SizedBox(width: 20), // Space between buttons
-                            Container(
-                              height: 50,
-                              width: 50, // Added width
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(50),
-                                color: Colors.black,
-                              ),
-                              child: Icon(
-                                Icons.apple,
-                                color: Colors.white,
-                              ), // Added icon
-                            ),
-                            SizedBox(width: 20),
-                            Container(
-                              height: 50,
-                              width: 50, // Set width
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(50),
-                                color: Colors.blue, // Facebook's primary color
-                              ),
-                              child: Center(
-                                child: Icon(
-                                  Icons.facebook, // Facebook icon
-                                  color: Colors.white,
-                                  size: 30, // Adjust size if needed
+                              const SizedBox(width: 15),
+                              GestureDetector(
+                                onTap: () => print("Google Sign-In clicked!"),
+                                child: SizedBox(
+                                  width: 40,
+                                  height: 40,
+                                  child: Image.asset("assets/google.png"),
                                 ),
                               ),
+                              const SizedBox(width: 15),
+                              GestureDetector(
+                                onTap: () => print("iCloud Sign-In clicked!"),
+                                child: SizedBox(
+                                  width: 40,
+                                  height: 40,
+                                  child: Image.asset("assets/apple.png"),
+                                ),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: 10),
+                          // Navigation to Sign Up
+                          TextButton(
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => const SignUpPage(),
+                                ),
+                              );
+                            },
+                            child: Text(
+                              "Create an account.",
+                              style:
+                                  TextStyle(fontSize: 16, color: myGreenColor),
                             ),
-                          ],
-                        ),
-                      ],
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
