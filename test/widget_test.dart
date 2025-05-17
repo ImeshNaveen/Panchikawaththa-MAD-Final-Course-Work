ef3bc0ebddf9fa629c5a075d1b16bad971ad5316
Pathum Theekshana <kptpremawansha@students.nsbm.ac.lk>
2025-05-17 13:36:15 +0530

    update settings page

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,14 +7,14 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:panchikawaththa/pages/home.dart';
+import 'package:panchikawaththa/pages/home_page.dart';
 
 import 'package:panchikawaththa/pages/login_page.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(homepage());
+    await tester.pumpWidget(HomePage());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
